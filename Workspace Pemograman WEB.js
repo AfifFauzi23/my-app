19990e6346974aa8c59d6ce848f29141d7aaeb48
AfifFauzi23 <afif1906.af@gmail.com>
2026-04-20 12:04:27 +0800

    feat: new learn page

--- Workspace Pemograman WEB.js
+++
@@ -1,2 +0,0 @@
-function tambah(a, b)
-{    return a + b;}console.log(tambah(5, 3)); // Output: 8
---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+export default function learnpage() {
+    return (
+        <div>
+            <div>
+                <img src="/palette.png" alt="Design" />
+                <p>Design</p>
+            </div>
+            <div>
+                <p>Figma</p>
+                <p>8 Hours Ago</p>
+                </div>
+            </div>
+    )
+}
+
